19acb041ad696e733fd240eac0c5862054d7cb55
Maikmm30 <maikmm30@gmail.com>
2020-04-22 17:10:16 -0600

    bAyuda

--- ProyectoFinal/Inventario/Inventario/Login.cs
+++ ProyectoFinal/Inventario/Inventario/Login.cs
@@ -157,7 +157,7 @@ namespace Inventario
 
         private void button1_Click_2(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("IExplore.exe", "D:/Users/Michael/Documents/Visual Studio 2019/ProyectoFinalDiseno/ProyectoFinal/Inventario/Inventario/PaginaWeb/indice.html");
+            System.Diagnostics.Process.Start("D:/Users/Michael/Documents/Visual Studio 2019/ProyectoFinalDiseno/ProyectoFinal/Inventario/Inventario/PaginaWeb/indice.html");
         }
     }
 }
